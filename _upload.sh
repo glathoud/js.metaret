aacf5fbd93c6fbc60ed26ec467404168d34c02f1
glathoud <glathoud@yahoo.fr>
2014-01-30 09:50:39 +0100

    misc

--- _upload.sh
+++ _upload.sh
@@ -5,6 +5,6 @@ echo '_upload.sh: uploading "metaret" article...'
 find . -name '*~' -exec rm {} \;
 echo "mkdir js.metaret
 cd js.metaret
-mput -rf *.scm *.html *.js *.TXT *.xcf *.jpg
+mput -rf *.scm *.html *.js *.TXT *.xcf *.jpg jsm_dev jsm_out jsm_out_build jsm_out_mini
 " | ncftp glat
 echo '_upload.sh: upload successful!'
