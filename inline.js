3cab8fd8fb271af3a481ddd45ec0134792afda42
glathoud <glathoud@yahoo.fr>
2014-02-01 09:53:53 +0100

    recognize the 3 types of inline statements

--- inline.js
+++ inline.js
@@ -1,4 +1,4 @@
-/*global need$ lightparse */
+/*global need$ load lightparse console*/
 
 if (typeof lightparse === 'undefined')
     (typeof need$ !== 'undefined'  ?  need$  :  load)( "lightparse.js" );
@@ -8,8 +8,19 @@ if (typeof lightparse === 'undefined')
     var       INLINE = 'inline'
     , LIGHTPARSE_OPT = { 
         extraReservedArr  : [ INLINE ]
-        , extraBracketArr : [ { open : INLINE, close : ';', typebracket : INLINE, ignore_unbalanced : true } ]
     }
+
+    , VARASSIGN = 'varassign'
+    ,    ASSIGN = 'assign'
+    ,      CALL = 'call'
+    
+    // xxx constants should be in a separate file
+    ,   BRACKET = 'bracket'
+    ,     ROUND = 'round'
+    , IDENTIFIER = 'identifier'
+    ,   RESERVED = 'reserved'
+    ,        VAR = 'var'
+    ,    VARDECL = 'vardecl'
     ;
     
     global.inline = inline;
@@ -21,12 +32,79 @@ if (typeof lightparse === 'undefined')
     // Returns another code string.
     {
         console.log('xxx inline', code.substring(0,100))
-        var lp = lightparse( code, LIGHTPARSE_OPT );
+        var      lp = lightparse( code, LIGHTPARSE_OPT )
+        ,       all = lp.all
+        , inlineArr = all
+            .map( function (o, ind) { return getInlineInfo( o, ind, all, code ); })
+            .filter( function (info) { return info; } )
+        ;
+        if (!inlineArr.length)
+            return code;
 
-        console.log('xxx inline lp')
-        console.log(lp);
+        console.log('xxx inline inlineArr', inlineArr)
 
         return code;
+    }
+
+    // --- Details
+
+    function getInlineInfo( o, ind, all, code )
+    {
+        if (o.name !== INLINE)
+            return;
+        
+        var v            // var        (optional)
+        ,   identifier   // identifier (optional)
+        ,   call         // call       (mandatory)
+        ,   args         // call args  (mandatory)
+        ;
+
+        if ((call = all[ ind+1 ]).type === CALL  &&  
+            (args = all[ ind+2 ]).type === BRACKET  &&  args.typebracket === ROUND)
+        {
+            return {
+                o            : o
+                , ind        : ind
+                , inlinetype : CALL
+                , call       : call
+                , args       : args
+            };
+        }
+
+        if ((identifier = all[ ind+1 ]).type === IDENTIFIER  &&  
+            (call = all[ ind+2 ]).type === CALL  &&  
+            (args = all[ ind+3 ]).type === BRACKET  &&  args.typebracket === ROUND  &&  
+            /=/.test( code.substring( identifier.end, call.begin )))
+        {
+            return {
+                o            : o
+                , ind        : ind
+                , inlinetype : ASSIGN
+                , identifier : identifier
+                , call       : call
+                , args       : args
+            };
+        }
+
+        var vc;
+        if ((v = all[ ind+1 ]).type === BRACKET   &&  v.typebracket === VAR  &&
+            (vc = v.children).length === 4  &&
+            (identifier = vc[ 1 ]).type === VARDECL  &&  
+            (call       = vc[ 2 ]).type === CALL  &&  
+            (args       = vc[ 3 ]).type === BRACKET  &&  args.typebracket === ROUND  &&  
+            /=/.test( code.substring( identifier.end, call.begin )))
+        {
+            return {
+                o            : o
+                , ind        : ind
+                , inlinetype : VARASSIGN
+                , identifier : identifier
+                , call       : call
+                , args       : args
+            };
+        }
+
+        throw new Error( 'Unrecognized inline syntax.' );
     }
 
 })(this);
