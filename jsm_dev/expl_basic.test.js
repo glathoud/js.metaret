5c2266b7cb095b9d08a0cb956d85f637ac272850
glathoud <glathoud@yahoo.fr>
2014-02-05 20:29:12 +0100

    split tests

--- jsm_dev/expl_basic.test.js
+++ jsm_dev/expl_basic.test.js
@@ -232,20 +232,6 @@
 
         //#END_TEST_DEV_ONLY
 
-        // --- sortedSearch: mutual recursion between 3 metafuns.
-
-        var sortedArray = [ 0, 0, 0, 0, 1, 1, 3, 4, 6, 6, 7, 8, 9, 10, 10, 10,
-                            11, 13, 14, 14, 15 ];
-        assert( '"0###3" === sortedSearch( v, 0 ).join("###")', sortedArray );
-        assert( '"4###5" === sortedSearch( v, 1 ).join("###")', sortedArray );
-        assert( 'null === sortedSearch( v, 2 )', sortedArray );
-        assert( '"8###9" === sortedSearch( v, 6 ).join("###")', sortedArray );
-        assert( 'null === sortedSearch( v, 12 ) ', sortedArray );
-        assert( '"18###19" === sortedSearch( v, 14 ).join("###")', sortedArray );
-        assert( '"20###20" === sortedSearch( v, 15 ).join("###")', sortedArray );
-
-
-
 
         // ---
 
