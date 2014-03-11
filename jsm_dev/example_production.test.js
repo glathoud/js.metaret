6a756be5c503797d4ec7de3108b58893379fe38e
glathoud <glathoud@yahoo.fr>
2014-03-11 21:19:02 +0100

    blah

---
+++ jsm_dev/example_production.test.js
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+<title>example: development</title>
+<script type="text/javascript" src="../js.metaret/metaret_standalone.js"></script>
+<script type="text/javascript">need$("jsm_dev/example_development.jsm");</script>
+</head>
+<body>
+<h1>example: development</h1>
+
+<p>GCD(910,1235)=<script type="text/javascript">document.write(gcd(910,1235));</script>
+
+<address>
+<a href="mailto:glathoud@yahoo.fr">gl</a>
+</address>
+</body>
+</html>
